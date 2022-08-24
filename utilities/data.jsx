126f1715ac17aab9620f484802ccd0fa09e3d77c
Kirk <pahtriq@outlook.com>
2022-08-24 17:37:10 +0300

    products list

---
+++ utilities/data.jsx
@@ -0,0 +1,77 @@
+const data = {
+    products: [
+        {
+            name: 'Free Shirt',
+            slug: 'free-shirt',
+            category: 'Shirts',
+            image: '/images/shirt1.jpg',
+            price: '1000',
+            brand: 'Nike',
+            rating: 4.8,
+            numReviews: 9,
+            countInStock: 22,
+            description: 'A very popular shirt'
+        },
+        {
+            name: 'Fit Shirt',
+            slug: 'fit-shirt',
+            category: 'Shirts',
+            image: '/images/shirt2.jpg',
+            price: '1000',
+            brand: 'Adidas',
+            rating: 4.8,
+            numReviews: 9,
+            countInStock: 22,
+            description: 'A very popular shirt'
+        },
+        {
+            name: 'Slim Shirt',
+            slug: 'slim-shirt',
+            category: 'Shirts',
+            image: '/images/shirt3.jpg',
+            price: '1000',
+            brand: 'Raymonds',
+            rating: 4.0,
+            numReviews: 5,
+            countInStock: 12,
+            description: 'A very popular shirt'
+        },
+        {
+            name: 'Golf Pants',
+            slug: 'golf-pants',
+            category: 'Pants',
+            image: '/images/pants1.jpg',
+            price: '1500',
+            brand: 'Raymonds',
+            rating: 8.0,
+            numReviews: 15,
+            countInStock: 32,
+            description: 'A very popular pants'
+        },
+        {
+            name: 'Fit Pants',
+            slug: 'fit-pants',
+            category: 'Pants',
+            image: '/images/pants2.jpg',
+            price: '1500',
+            brand: 'Adidas',
+            rating: 5.0,
+            numReviews: 5,
+            countInStock: 2,
+            description: 'A very popular pants'
+        },
+        {
+            name: 'Classic Pants',
+            slug: 'classic-pants',
+            category: 'Pants',
+            image: '/images/pants3.jpg',
+            price: '1000',
+            brand: 'Buchenes',
+            rating: 9.0,
+            numReviews: 13,
+            countInStock: 12,
+            description: 'A very popular pants'
+        },
+    ]
+}
+export default data;
